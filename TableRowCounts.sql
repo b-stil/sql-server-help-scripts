949541c4d89608878636218a4d279c8989cb8179
elbean <elbean@users.noreply.github.com>
2019-04-29 09:35:38 -0600

    Update to row count script

--- TableRowCounts.sql
+++ TableRowCounts.sql
@@ -1,10 +1,16 @@
-
-SELECT 
-	SCHEMA_NAME(t.schema_id) AS SchemaName 
-	, t.name AS TableName
-	,i.rowcnt AS Rows
-FROM sys.tables t
-INNER JOIN sys.sysindexes i ON t.object_id = i.id
-WHERE i.indid = 1
-
-
+/* Count based on number of rows for the main index on the table */
+;WITH base AS (
+	SELECT 
+		SCHEMA_NAME(t.schema_id) AS SchemaName 
+		,t.[name] AS TableName
+		,i.rowcnt AS NumRows
+		,ROW_NUMBER() OVER (PARTITION BY i.id ORDER BY i.indid ASC) AS rn
+	FROM sys.tables t
+	INNER JOIN sys.sysindexes i ON t.object_id = i.id
+)
+SELECT
+	b.SchemaName
+	,b.TableName
+	,b.NumRows
+FROM base b
+WHERE b.rn = 1;
